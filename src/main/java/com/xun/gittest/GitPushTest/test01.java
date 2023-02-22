2228a00c250a668837a5d8c5e3053f5aa93f63b8
sakura <1264569162@qq.com>
2023-02-22 16:03:55 +0800

    第二次提交

--- src/main/java/com/xun/gittest/GitPushTest/test01.java
+++ src/main/java/com/xun/gittest/GitPushTest/test01.java
@@ -3,5 +3,6 @@ package com.xun.gittest.GitPushTest;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        System.out.println("第二次helloworld");
     }
 }
