3e2db64edcb8f9254a97d5aeb9c4dfd36dd70be9
sakura <1264569162@qq.com>
2023-02-22 17:04:54 +0800

    master的合并冲突01

--- src/main/java/com/xun/gittest/GitPushTest/test01.java
+++ src/main/java/com/xun/gittest/GitPushTest/test01.java
@@ -5,5 +5,6 @@ public class test01 {
         System.out.println("helloworld");
         System.out.println("第二次helloworld");
         System.out.println("hot-fix第一次helloworld");
+        System.out.println("master的合并冲突");
     }
 }
